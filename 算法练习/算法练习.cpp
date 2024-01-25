9673b18082560dfd4643ff55f4a799421a21c045
shikai-123 <454283985@qq.com>
2024-01-25 14:45:19 +0800

    309.最佳买卖股票时机含冷冻期

--- 算法练习/算法练习.cpp
+++ 算法练习/算法练习.cpp
@@ -7023,12 +7023,40 @@ namespace DynamicPlanning
 			https://www.programmercarl.com/0309.%E6%9C%80%E4%BD%B3%E4%B9%B0%E5%8D%96%E8%82%A1%E7%A5%A8%E6%97%B6%E6%9C%BA%E5%90%AB%E5%86%B7%E5%86%BB%E6%9C%9F.html#%E7%AE%97%E6%B3%95%E5%85%AC%E5%BC%80%E8%AF%BE
 		思路：
 			dp[i][0]：状态一，持有股票状态（今天买入股票，或者是之前就买入了股票然后没有操作，一直持有）
+				dp[i-1][0]:前一天也是保持持有
+				dp[i-1][3]-prices[i]:前一天是冷静
+				dp[i-1][1]-prices[i]:前一天是保持卖出
+
 			dp[i][1]：状态二，保持卖出股票的状态（两天前就卖出了股票，度过一天冷冻期。或者是前一天就是卖出股票状态，一直没操作）
+				dp[i-1][1]:前一天是保持卖出
+				dp[i-1][3]:前一天是冷冻期状态
+
 			dp[i][2]：状态三，今天卖出股票
+				dp[i-1][2]:前一天也是卖出股票
+				dp[i-1][0]+prices[i]:前一天是保持持有
+
 			dp[i][3]：状态四，今天为冷冻期状态，但冷冻期状态不可持续，只有一天！
+				dp[i-1][3]:前一天是冷静期状态
+				dp[i-1][2]:前一天卖出股票
+
+			这个题目的特点是，卖出后有一天的冷静期
 		*/
 		int maxProfitBest(vector<int>& prices) {
+			if (prices.size() == 0) return 0;
+			vector<vector<int>>dp(prices.size(), { 0,0,0,0 });
 
+			dp[0][0] = -prices[0];
+			dp[0][1] = 0;//保持卖出股票的状态；第一天买股票，然后第一天卖了利润是0
+			dp[0][2] = 0;//今天卖出股票,第一天买股票，然后第一天卖了利润是0
+			dp[0][3] = 0;//今天为冷冻期状态，冷静期也是卖了。总是都是一样，利润也是0；
+			for (size_t i = 1; i < prices.size(); i++)
+			{
+				dp[i][0] = max(dp[i - 1][0], max(dp[i - 1][3] - prices[i], dp[i - 1][1] - prices[i]));
+				dp[i][1] = max(dp[i - 1][1], dp[i - 1][3]);
+				dp[i][2] = max(dp[i - 1][2], dp[i - 1][0] + prices[i]);
+				dp[i][3] = max(dp[i - 1][3], dp[i - 1][2]);
+			}
+			return max(dp[prices.size() - 1][1], max(dp[prices.size() - 1][2], dp[prices.size() - 1][3]));
 		}
 
 		/*
