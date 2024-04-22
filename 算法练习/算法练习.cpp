fc4e56ee12d28e06f4afa0f3163aabe644fd6f18
shikai-123 <454283985@qq.com>
2024-04-22 17:45:42 +0800

    153. 寻找旋转排序数组中的最小值-没弄完

--- 算法练习/算法练习.cpp
+++ 算法练习/算法练习.cpp
@@ -12386,6 +12386,13 @@ namespace ERFENCAHZHAO {
 
 		/*
 		153. 寻找旋转排序数组中的最小值
+		题意:
+			已知一个长度为 n 的数组，预先按照升序排列，经由 1 到 n 次 旋转 后，得到输入数组。例如，原数组 nums = [0,1,2,4,5,6,7] 在变化后可能得到：
+			若旋转 4 次，则可以得到 [4,5,6,7,0,1,2]
+			若旋转 7 次，则可以得到 [0,1,2,4,5,6,7]
+			给你一个元素值 互不相同 的数组 nums ，它原来是一个升序排列的数组，并按上述情形进行了多次旋转。请你找出并返回数组中的 最小元素 。
+			你必须设计一个时间复杂度为 O(log n) 的算法解决此问题。
+
 		参考：
 			https://leetcode.cn/problems/find-minimum-in-rotated-sorted-array/solutions/2680224/er-fen-cha-zhao-yu-feng-zhi-bi-jiao-by-w-kanl/?envType=study-plan-v2&envId=top-100-liked
 			https://www.programmercarl.com/0704.%E4%BA%8C%E5%88%86%E6%9F%A5%E6%89%BE.html#%E6%80%9D%E8%B7%AF
@@ -12406,9 +12413,7 @@ namespace ERFENCAHZHAO {
 				if (nums[mid] > p)
 					left = mid + 1;
 				else
-				{
 					right = mid - 1;
-				}
 			}
 			return nums[left];//最后返回nums[left]
 		}
