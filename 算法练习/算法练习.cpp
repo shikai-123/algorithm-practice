b3f3e043d9a342a3097d83e338828dc85762430a
shikai-123 <454283985@qq.com>
2024-04-22 14:28:04 +0800

    74. 搜索二维矩阵, 240. 搜索二维矩阵 II,二刷.

--- 算法练习/算法练习.cpp
+++ 算法练习/算法练习.cpp
@@ -2019,6 +2019,9 @@ namespace Matrix
 		240. 搜索二维矩阵 II
 		74. 搜索二维矩阵
 		这两个题一个答案
+		题意:
+			给你一个满足下述两条属性的 m x n 整数矩阵：每行中的整数从左到右按非严格递增顺序排列。每行的第一个整数大于前一行的最后一个整数。
+			给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false 。
 		参考：
 			无
 		思路：
@@ -2039,7 +2042,18 @@ namespace Matrix
 			return false;
 		}
 
-
+		//74. 搜索二维矩阵,	240. 搜索二维矩阵 II,二刷.
+		bool searchMatrix(vector<vector<int>>& matrix, int target) {
+			for (size_t i = 0; i < matrix.size(); i++) {
+				for (size_t l = 0; l < matrix[0].size(); l++) {
+					if (matrix[i][l] == target)
+						return true;
+					else if (matrix[i][matrix[0].size() - 1] < target)
+						break;
+				}
+			}
+			return false;
+		}
 
 
 
