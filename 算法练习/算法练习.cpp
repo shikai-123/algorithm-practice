f5c530add062a121c1e399f162e3e5393ca33a5b
shikai-123 <454283985@qq.com>
2024-04-22 17:38:35 +0800

    33. 搜索旋转排序数组,二刷

--- 算法练习/算法练习.cpp
+++ 算法练习/算法练习.cpp
@@ -12373,9 +12373,9 @@ namespace ERFENCAHZHAO {
 				else if (nums[mid] <= nums[r]) {
 
 					if (nums[mid] <= target && target <= nums[r])
-						r = mid - 1;
-					else
 						l = mid + 1;
+					else
+						r = mid - 1;
 				}
 			}
 			return -1;
