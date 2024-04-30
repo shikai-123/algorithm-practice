58dd04ecbdebffb3da521d8b72ed2807e30af705
shikai-123 <454283985@qq.com>
2024-04-30 17:46:05 +0800

    5. 最长回文子串--更新

--- 算法练习/算法练习.cpp
+++ 算法练习/算法练习.cpp
@@ -10871,6 +10871,9 @@ namespace DynamicPlanning
 
 		/*
 		5. 最长回文子串
+		题意:
+			给你一个字符串 s，找到 s 中最长的回文子串。
+			如果字符串的反序与原始字符串相同，则该字符串称为回文字符串。
 		参考：
 			https://leetcode.cn/problems/longest-palindromic-substring/solutions/7600/5-zui-chang-hui-wen-zi-chuan-cc-by-bian-bian-xiong/?envType=study-plan-v2&envId=top-100-liked
 		思路：
