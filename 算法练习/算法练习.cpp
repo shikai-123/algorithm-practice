e3b07fb5dc49b3fd17c2191c149a3cb93895186d
shikai-123 <454283985@qq.com>
2024-07-05 16:19:08 +0800

    11. 盛最多水的容器 修改

--- 算法练习/算法练习.cpp
+++ 算法练习/算法练习.cpp
@@ -1103,7 +1103,7 @@ namespace DoublePointer
 				else if (height[L] > height[R]) {//如果左边大于右边，移动右边
 					R--;
 				}
-				else if (true) {//如果右边大于左边，移动左边
+				else {//如果右边大于左边，移动左边
 					L++;
 				}
 			}
@@ -12986,7 +12986,35 @@ namespace ERFENCAHZHAO {
 			两个都是有序数组。
 		*/
 		double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+			int n = nums1.size();
+			int m = nums2.size();
 
+			if (n > m)  //保证数组1一定最短
+			{
+				return findMedianSortedArrays(nums2, nums1);
+			}
+
+			// Ci 为第i个数组的割,比如C1为2时表示第1个数组只有2个元素。LMaxi为第i个数组割后的左元素。RMini为第i个数组割后的右元素。
+			int LMax1, LMax2, RMin1, RMin2, c1, c2, lo = 0, hi = 2 * n;  //我们目前是虚拟加了'#'所以数组1是2*n长度
+
+			while (lo <= hi)   //二分
+			{
+				c1 = (lo + hi) / 2;  //c1是二分的结果
+				c2 = m + n - c1;
+
+				LMax1 = (c1 == 0) ? INT_MIN : nums1[(c1 - 1) / 2];
+				RMin1 = (c1 == 2 * n) ? INT_MAX : nums1[c1 / 2];
+				LMax2 = (c2 == 0) ? INT_MIN : nums2[(c2 - 1) / 2];
+				RMin2 = (c2 == 2 * m) ? INT_MAX : nums2[c2 / 2];
+
+				if (LMax1 > RMin2)
+					hi = c1 - 1;
+				else if (LMax2 > RMin1)
+					lo = c1 + 1;
+				else
+					break;
+			}
+			return (max(LMax1, LMax2) + min(RMin1, RMin2)) / 2.0;
 		}
 
 
